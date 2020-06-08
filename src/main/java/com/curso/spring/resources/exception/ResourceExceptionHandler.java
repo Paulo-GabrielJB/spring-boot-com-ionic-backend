370a83eb0a24111be159e37ed4a8b4a566f4219c
Paulo Gabriel <pgj1085@gmail.com>
2020-06-08 16:18:31 -0300

    Personalizando resposta da validacao

--- src/main/java/com/curso/spring/resources/exception/ResourceExceptionHandler.java
+++ src/main/java/com/curso/spring/resources/exception/ResourceExceptionHandler.java
@@ -6,6 +6,8 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -40,4 +42,16 @@ public class ResourceExceptionHandler {
         return ResponseEntity.status(status).body(standardError);
     }
 	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	 public ResponseEntity<StandardError> badRequest(MethodArgumentNotValidException e, HttpServletRequest request){
+        String error = "Bad request";
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        ValidationError validatioError = new ValidationError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+        
+        for(FieldError x: e.getBindingResult().getFieldErrors())
+        	validatioError.addError(x.getField(), x.getDefaultMessage());
+        	
+        
+        return ResponseEntity.status(status).body(validatioError);
+    }
 }
